6c6fc8308bb9972fdbf8ee9e9bd4cd62ade872f0
Falcovscaia_Iulia <panocea99@gmail.com>
2023-02-27 00:03:42 +0100

    initial commit_1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 
 public class Main {
+    public static int cisla(int a,int b) {
+        if (a > b) {
+            return 1;
+        }
+        if (a < b) {
+            return -1;
+        }
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 3;
+        int b = 3;
+        int c = cisla(a,b);
+        System.out.println("Hello world!\n");
+        System.out.println(c);
+
     }
 }
